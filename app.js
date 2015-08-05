76c0a8128cbb2ecc9d2660316c937eac89315f7a
Ubuntu <ubuntu@ip-172-31-5-220.eu-central-1.compute.internal>
2015-08-05 16:08:21 +0000

    Added app.js file to use with Node

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+  console.log('Server was hit');
+});
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendfile( __dirname + req.params[0]); 
+ });
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
